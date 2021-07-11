1cef8e03137b9b7ffd02e20402cfa976dc2f4154
Kang Seonghyeon <shyeon@gmail.com>
2021-07-11 20:23:18 +0900

    first chapter

---
+++ app/main.py
@@ -0,0 +1,53 @@
+import requests
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+db = []
+URL = "http://worldtimeapi.org/api/timezone"
+
+
+class City(BaseModel):
+    name: str
+    timezone: str
+
+
+@app.get("/")
+def root():
+    return {"Hello": "World!"}
+
+
+@app.get("/cities")
+def get_cities():
+    results = []
+    for city in db:
+        url = f"{URL}/{city['timezone']}"
+        r = requests.get(
+            url,
+        )
+        cur_time = r.json()["datetime"]
+        results.append(
+            {"name": city["name"], "timezon": city["timezone"], "current_time": cur_time}
+        )
+    return results
+
+
+@app.get("/cities/{citi_id}")
+def get_city(city_id: int):
+    city = db[city_id - 1]
+    url = f"{URL}/{city['timezone']}"
+    r = requests.get(url)
+    cur_time = r.json()["datetime"]
+    return {"name": city["name"], "timezon": city["timezone"], "current_time": cur_time}
+
+
+@app.post("/cities")
+def create_city(city: City):
+    db.append(city.dict())
+    return db[-1]
+
+
+@app.delete("/cities/{citi_id}")
+def delete_city(city_id: int):
+    db.pop(city_id - 1)
+    return {}
